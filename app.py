9ecd1a2991c205b736ac6066e3e25cbf4640b9ee
maftuhlutfi <playmakernyomlo@gmail.com>
2022-01-12 16:41:44 +0700

    create home

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 app.config['SECRET_KEY'] = 'flask-book-movie-kelompok'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/<string:book_id>')
+def book(book_id):
+    return render_template('book.html')
